29023400ea79f6a828cca48f4fb27bd670e66937
Kim Kurikan <kim.kurikan@gmail.com>
2025-03-15 10:52:13 +0200

    added user login ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react"
-import HomePage from "./components/HomePage"
+//import HomePage from "./components/HomePage"
+import SignInModal from "./components/SignInModal";
 
 function App() {
   const [displayList, setDisplayList] = useState(true);
 
   return (
     <div className="m-5">
-      {displayList && <HomePage />}
+      {displayList && <SignInModal />}
       <button className="btn btn-primary" onClick={() => { setDisplayList(!displayList) }}>Toggle</button>
     </div>
 
